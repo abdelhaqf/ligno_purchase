4859880ae353283e3d646a0ef8b7fad3f1b2f924
DICKY IQBAL LUBIS <loebiez@gmail.com>
2020-10-16 17:17:02 +0700

    dicky-up

--- src/views/PriceList.vue
+++ src/views/PriceList.vue
@@ -1,82 +1,110 @@
 <template>
-  <div class="">
-    <div class="row q-pa-md">
-      <q-select class="col-4" 
-       outlined dense
-       v-model="selectOption" :options="option" 
-       map-options emit-value label="Product"
-       @input="change()"
-        />
-    </div>
-     <q-markup-table flat dense  square>
-       <thead class="bg-green text-white">
-         <th>PO</th>
-         <th>PO Date</th>
-         <th>Vendor</th>
-         <th>Price</th>
-         <th>Qty</th>
-       </thead>
-       <tbody>
-         <tr v-for="p in priceList" :key="p.id">
-           <td>{{p.po_id}}</td>
-           <td>{{p.po_date}}</td>
-           <td>{{p.vendor}}</td>
-           <td>{{setCurrency(parseFloat(p.price)/parseFloat(p.qty), p.currency)}} / {{p.unit}}</td>
-           <td>{{p.qty}}</td>
-         </tr>
-       </tbody>
-     </q-markup-table>
+  <div class="row  relative q-px-lg q-pt-lg">
+    <q-card class="col-12 bg-white rounded-borders">
+      <q-card-section class="row">
+        <q-select
+          outlined dense
+          v-model="selectOption"
+          map-options emit-value
+          use-input
+          hide-selected
+          fill-input
+          input-debounce="0"
+          :options="filtered"
+          @filter="filterOP"
+          label="Nama Barang"
+          class="col-3"
+          @input="change()"
+        >
+          <template v-slot:no-option>
+            <q-item>
+              <q-item-section class="text-grey">
+                No results
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
+        
+      </q-card-section>
+      <q-markup-table flat dense square>
+        <thead class="bg-green-4 text-white">
+          <th class="text-left">PO</th>
+          <th class="text-left">PO Date</th>
+          <th class="text-left">Vendor</th>
+          <th class="text-right">Price</th>
+          <th class="text-right">Qty</th>
+        </thead>
+        <tbody v-if="priceList.length" class="bg-green-1">
+          <tr v-for="p in priceList" :key="p.id">
+            <td class="text-left">{{ p.po_id }}</td>
+            <td class="text-left">{{ p.po_date }}</td>
+            <td class="text-left">{{ p.vendor }}</td>
+            <td class="text-right">{{ setCurrency(parseFloat(p.price) / parseFloat(p.qty), p.currency) }} / {{ p.unit }}</td>
+            <td class="text-right">{{ p.qty }}</td>
+          </tr>
+        </tbody>
+        <tbody v-else class="bg-green-1">
+          <tr>
+            <td class="text-center text-grey" colspan="99">
+              tidak ada data, atau mungkin anda belum memilih
+            </td>
+          </tr>
+        </tbody>
+        <q-card-section></q-card-section>
+      </q-markup-table>
+    </q-card>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-      option: [],
+      option: [], filtered: [],
       selectOption: null,
       priceList: [],
     };
   },
-  mounted(){
-    this.fetchData()
+  mounted() {
+    this.fetchData();
   },
-  methods:{
-    fetchData(){
-      this.$http.get('/list_item', {})
-      .then (result => {
-        this.option = result.data
-      })
+  methods: {
+    fetchData() {
+      this.$http.get("/list_item", {}).then((result) => {
+        this.option = result.data;
+      });
     },
-    change(val){
-      this.$http.get('/pricelist/' + this.selectOption, {})
-      .then (result => {
-        this.priceList = result.data
-      })
+    change(val) {
+      this.$http.get("/pricelist/" + this.selectOption, {}).then((result) => {
+        this.priceList = result.data;
+      });
     },
     setCurrency(price, cur) {
-      if(cur == 'IDR'){
-        const formatter = new Intl.NumberFormat('ID', {
-          style: 'currency',
-          currency: 'IDR',
+      if (cur == "IDR") {
+        const formatter = new Intl.NumberFormat("ID", {
+          style: "currency",
+          currency: "IDR",
           currencyDisplay: "symbol",
-          minimumFractionDigits: 0
-        })
-        return formatter.format(price)
-      }
-      else if (cur == 'USD'){
-        const formatter = new Intl.NumberFormat('US', {
-          style: 'currency',
-          currency: 'USD',
+          minimumFractionDigits: 0,
+        });
+        return formatter.format(price);
+      } else if (cur == "USD") {
+        const formatter = new Intl.NumberFormat("US", {
+          style: "currency",
+          currency: "USD",
           currencyDisplay: "symbol",
-          minimumFractionDigits: 2
-        })
-        return formatter.format(price)
+          minimumFractionDigits: 2,
+        });
+        return formatter.format(price);
       }
     },
-    
-  }
+    filterOP (val, update, abort) {
+      update(() => {
+        const needle = val.toLowerCase()
+        this.filtered = this.option.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    },
+  },
 };
 </script>
 
--- src/views/SPPCreate.vue
+++ src/views/SPPCreate.vue
@@ -1,47 +1,107 @@
 <template>
-  <div class="row">
-    <div class="col-6">
-      <div class="bg-green text-white text-h6 q-pa-md">SPP Baru</div>
-      <div class="q-pa-md q-gutter-md ">
-        <q-input outlined v-model="spp.item" label="Item" stack-label v-if="showInput" dense >
+  <div class="row justify-center q-py-lg rounded-borders">
+    <q-card class=" bg-white">
+      <q-card-section class="bg-green-4 text-white text-h6">SPP Baru</q-card-section>
+      <!-- form  -->
+      <q-card-section class="q-gutter-md ">
+        <div class="row">
+
+            <q-input 
+              outlined v-model="spp.item" label="Nama Barang"  class="col-10"
+              stack-label v-if="showInput" dense
+              />
+            <q-select v-else
+              outlined dense
+              v-model="spp.item"
+              map-options emit-value
+              use-input
+              hide-selected
+              fill-input
+              input-debounce="0"
+              :options="filtered"
+              @filter="filterOP"
+              label="Nama Barang"
+              @input="select" class="col-10"
+            >
+              <template v-slot:no-option>
+                <q-item>
+                  <q-item-section class="text-grey">
+                    No results
+                  </q-item-section>
+                </q-item>
+              </template>
+            </q-select>
+            <q-toggle
+              v-model="showInput"
+              color="green" icon="add"
+              keep-color
+            />
+        </div>
+        <!-- <q-input outlined v-model="spp.item" label="Nama Barang" stack-label v-if="showInput" dense>
           <template v-slot:append>
             <q-btn round dense flat icon="arrow_drop_down" no-caps @click="showInput = !showInput" />
           </template>
         </q-input>
-        <q-select class="col-4" v-else
-          outlined dense stack-label
-          v-model="spp.item" :options="option" 
-          map-options emit-value label="Item"
-          >
+        <q-select
+          class="col-4"
+          v-else
+          outlined
+          dense
+          stack-label
+          v-model="spp.item"
+          :options="option"
+          map-options
+          emit-value
+          label="Nama Barang"
+        >
           <template v-slot:option="scope">
-            <q-item
-              v-bind="scope.itemProps"
-              v-on="scope.itemEvents"
-            >
-              <q-item-section class="text-grey text-italic" v-if="scope.opt.value == ''" @click="test()"  >
-                {{scope.opt.label}}
+            <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+              <q-item-section class="text-grey text-italic" v-if="scope.opt.value == ''" @click="test()">
+                {{ scope.opt.label }}
               </q-item-section>
-              <q-item-section v-else >
-                {{scope.opt.label}}
+              <q-item-section v-else>
+                {{ scope.opt.label }}
               </q-item-section>
             </q-item>
           </template>
-        </q-select>
+        </q-select> -->
 
         <div>
           <div class="row q-col-gutter-sm">
             <q-input type="number" class="col-7" outlined v-model="spp.qty" label="Qty" stack-label dense />
-            <q-input class="col-5" outlined v-model="spp.unit" label="Unit" stack-label dense />
+            <q-input class="col-5" outlined v-model="spp.unit" label="Satuan (kg/ml/buah)" stack-label dense />
           </div>
         </div>
-        <q-input outlined v-model="spp.deadline" label="Deadline" stack-label dense readonly />
-        <q-date v-model="spp.deadline" minimal :options="limitDate" />
-        <q-input outlined v-model="spp.description" label="Description" stack-label dense type="textarea" />
-        <div class="q-gutter-md row justify-end">
-          <q-btn color="primary" label="Submit" @click="createSPP()" />
-        </div>
-      </div>
-    </div>
+        <!-- old qdate  -->
+        <!-- <q-input outlined v-model="spp.deadline" label="Deadline" stack-label dense readonly />
+        <q-date v-model="spp.deadline" minimal :options="limitDate" /> -->
+        <!-- new qdate  -->
+        <q-input outlined v-model="spp.deadline" mask="date" :rules="['date']" label="Deadline (tahun/bulan/tanggal)">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                <q-date minimal v-model="spp.deadline">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+
+        <q-input
+          outlined
+          v-model="spp.description"
+          class="q-mb-lg"
+          label="Keterangan"
+          stack-label
+          dense
+          type="textarea"
+        />
+      </q-card-section>
+      <q-btn class="full-width no-border-radius" color="primary" label="Ajukan" @click="createSPP()" />
+    </q-card>
   </div>
 </template>
 
@@ -56,52 +116,55 @@ export default {
         deadline: moment()
           .add(1, "days")
           .format("YYYY/MM/DD"),
+          item: ''
       },
       showInput: false,
-      option: [],
+      option: [], filtered: []
     };
   },
-  mounted(){
-      this.$http.get('/list_item', {})
-      .then (result => {
-        this.option = result.data
-        this.option.unshift({
-          value: '',
-          label: ' input baru'
-        })
-      })
+  mounted() {
+    this.$http.get("/list_item", {}).then((result) => {
+      this.option = result.data;
+      // this.option.unshift({
+      // value: "",
+      // label: " input baru",
+      // });
+    });
   },
   methods: {
-    test(){
-      this.showInput = true
-    },
+    // test() {
+    //   this.showInput = true;
+    // },
     limitDate(date) {
       return date >= moment().format("YYYY/MM/DD");
     },
-    createSPP(){
-      this.spp.user_id = this.$store.state.currentUser.user_id
-      this.$http.post('/new_spp', this.spp, {})
-      .then (result => {
+    createSPP() {
+      this.spp.user_id = this.$store.state.currentUser.user_id;
+      this.$http.post("/new_spp", this.spp, {}).then((result) => {
         this.spp = {
           deadline: moment()
             .add(1, "days")
             .format("YYYY/MM/DD"),
-        }
-        // this.spp = result.data
+            item: ''
+        };
 
         var history = {
           spp_id: result.data,
-              status: 'created',
-              content: 'Dibuat oleh ' + this.$store.state.currentUser.username
-        }
-        this.$http.post('/new_history', history, {})
-        .then (result => {
-          })
-        
-        this.$q.notify('SPP Berhasil Dibuat!')
+          status: "created",
+          content: "Dibuat oleh " + this.$store.state.currentUser.username,
+        };
+        this.$http.post("/new_history", history, {}).then((result) => {});
+
+        this.$q.notify("SPP Berhasil Dibuat!");
         // this.$router.push('/spp/list')
+      });
+    },
+    filterOP (val, update, abort) {
+      update(() => {
+        const needle = val.toLowerCase()
+        this.filtered = this.option.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
       })
-    }
+    },
   },
 };
 </script>
